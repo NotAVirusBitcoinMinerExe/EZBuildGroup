929b409459c6d1efd1363be4835895090ea60233
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-07-02 16:29:21 +0300

    отформатировал код с помощью prettier

--- jsphoto.js
+++ jsphoto.js
@@ -1,34 +1,34 @@
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+	showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+	showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
-  var i = 1;
-  var slides = document.getElementsByClassName("mySlides");
-  console.log(slides);
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+	let i = 1;
+	const slides = document.getElementsByClassName('mySlides');
+	console.log(slides);
+	const dots = document.getElementsByClassName('dot');
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = 'none';
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(' active', '');
+	}
 
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+	slides[slideIndex - 1].style.display = 'block';
+	dots[slideIndex - 1].className += ' active';
 }
